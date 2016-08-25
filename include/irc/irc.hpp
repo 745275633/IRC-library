f893a09ed4e3062fb8473c45b648f4c4213205e7
745275633 <7452756333@163.com>
2016-08-25 14:55:32 +0800

    真·第一步

--- include/irc/irc.hpp
+++ include/irc/irc.hpp
@@ -1,8 +1,10 @@
 /*!
  * \file irc/irc.hpp
- * \brief 定义了irc的一些基本信息
+ * \brief 定义了irc_class的一些基本信息
  * \author 吞噬黑洞的数据（745275633）
  */
+
+#include <boost/smart_ptr.hpp>
 
 /// \brief DA技术工作室命名空间
 namespace DA
@@ -12,6 +14,6 @@ namespace DA
 class irc
 {
 protected:
-
+    boost::shared_ptr<tcp::socket> socket;
 }
 }
